0c2e6e9287234d783e5c3ed5e8d3c4a369569b50
kill84 <71645149+kill84@users.noreply.github.com>
2020-11-23 19:59:13 +0100

    mailer configured for heroku-dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,6 +115,6 @@ Rails.application.configure do
 
   #  config.action_mailer.delivery_method = :letter_opener
     config.action_mailer.perform_deliveries = true
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.default_url_options = { host: 'https://ankanart-dev.herokuapp.com/'}
 
 end
