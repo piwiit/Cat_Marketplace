6b2b85f6b691eb14640bb46c208f1334c27f691f
kill84 <71645149+kill84@users.noreply.github.com>
2020-11-24 07:51:28 +0100

    configuration du mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   #  config.action_mailer.delivery_method = :letter_opener
-  #  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'https://ankanart-dev.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -112,9 +112,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  #  config.action_mailer.delivery_method = :letter_opener
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.default_url_options = { host: 'https://ankanart-dev.herokuapp.com/'}
 
 end
