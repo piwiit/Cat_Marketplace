64543d9af3ac344f60248a859a2550bedbb6b1cf
Sabatol <u.sacrez@gmail.com>
2020-11-25 12:13:10 +0100

    Update ugo 12h12

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-    def log_in(user)
-        session[:user_id] = user.id 
-    end
-
-    def current_user
-        User.find_by(id: session[:user_id])
-      end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord #  :lockable, :timeoutable, :trackable and :omnia
          :registerable,
          :recoverable,
          :rememberable,
-         :confirmable,
          :validatable
   has_many :carts
   validates_presence_of :firstname, :lastname, :age
