28bc191a228bdea16f2567710d0e47dc13fb27af
Sabatol <u.sacrez@gmail.com>
2020-11-25 12:27:10 +0100

    Update ugo 12h20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-=======
-  include SessionHelper
->>>>>>> master
   
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
