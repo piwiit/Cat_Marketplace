47aedb77e3fea08e866a4377c908092c833518a4
ChriZoizo <christeufeur83@gmail.com>
2020-11-26 17:11:43 +0100

    26/11/2020 17:11

--- app/models/join_art_cart.rb
+++ app/models/join_art_cart.rb
@@ -4,11 +4,7 @@ class JoinArtCart < ApplicationRecord
   validates :artwork, presence: true
   validates :cart, presence: true
 
-<<<<<<< HEAD
-
-=======
   def price_artwork
     price = artwork.price
   end
->>>>>>> b8b254f5a54b415d3e0742a4272f64bf2698ea4d
 end
