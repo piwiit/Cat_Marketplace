b8b254f5a54b415d3e0742a4272f64bf2698ea4d
piwiit <bouillaux@hotmail.fr>
2020-11-26 12:06:46 +0100

    add toto thx chris best team ever !

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,9 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :join_art_carts
-
-
   has_many :artworks, through: :join_art_carts
 
-
-
+  def total
+    join_art_carts.to_a.sum(&:price_artwork)
+  end
 end
