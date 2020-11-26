9b3ab105891bda734b4c97b318a3907ad41d6494
FlorianEmpinet <fempinet@gmail.com>
2020-11-26 13:53:13 +0100

    Modify NavBar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,34 @@
 module ApplicationHelper
+  
+  def find_user_cart
+  @cart = Cart.find(current_user.id)
+  array = @cart.join_art_carts.to_a
+  @art = []
+  array.each do |n| 
+    @art << Artwork.find(n.artwork_id)
+  end 
+
+
+  # Array each do 
+  return @art
+  end
+
+  def find_total_price
+    @cart = Cart.find(current_user.id)
+    array = @cart.join_art_carts.to_a
+    @art = []
+    array.each do |n| 
+      @art << Artwork.find(n.artwork_id)
+    end 
+  @amount = 0
+    @art.each do |p|
+      @amount += p.price
+    end 
+    # Array each do 
+    return @amount
+    end
+
+
   def resource_name
     :user
   end
