da72fc93245c192c027454f6a9f63f05c64ebd12
Sabatol <u.sacrez@gmail.com>
2020-11-26 19:51:22 +0100

    commit de l'espoir 2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,35 +8,43 @@ class CategoriesController < ApplicationController
   end
 
   def new
-@category = Categories.new
+    @category = Categories.new
   end
 
 
   def create
-  @category.new = Categories.new(name: params[:name])
-  if @category.save
-    redirect_back(fallback_location: root_path)   
-flash[:notice_good] = "Category Crée"
-  else 
-    flash[:notice_bad] = "Creation avortée"
-    render 'new'
+    @category.new = Categories.new(name: params[:name])
+    if @category.save
+      redirect_back(fallback_location: root_path)   
+      flash[:notice_good] = "Category Crée"
+    else 
+      flash[:notice_bad] = "Creation avortée"
+      render 'new'
     end 
   end
 
   def edit
-@category_edit = Categories.find(params[:id])
+    @category_edit = Categories.find(params[:id])
   end
 
   def update
-@category = Categories.find(params[:id])
-category_params = params.require(:category).permit(:name)
-@category.update
+    @category = Categories.find(params[:id])
+    category_params = params.require(:category).permit(:name)
+    @category.update
 
-
-end
+    if @category.update(post_params)
+      redirect_to root_path
+      flash[:notice_good] = "Edition reussis !"
+    else 
+      redirect_to gossip_path(id)
+      flash[:notice_bad] = "Probleme rencontré"
+    end
+  end
 
 
   def destroy
-
+    @category = Categories.find(params[:id])
+    @category.destroy
+    redirect_to root_path
   end
 end
