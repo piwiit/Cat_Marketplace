bc3a478a6a75bcf23b6837e3ecb3456324f52af8
kill84 <71645149+kill84@users.noreply.github.com>
2020-11-26 18:45:04 +0100

    change view stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,24 +4,26 @@ class ChargesController < ApplicationController
   end
 
   def create
-    cart = Cart.find_by(user_id: current_user.id)
+    @cart = Cart.find_by(user_id: current_user.id)
 
-    customer =
+    @customer =
       Stripe::Customer.create(
         { email: params[:stripeEmail], source: params[:stripeToken] }
       )
 
-    charge =
+    @charge =
       Stripe::Charge.create(
         {
-          customer: customer.id,
-          amount: (cart.total * 100).to_i,
+          customer: @customer.id,
+          amount: (@cart.total * 100).to_i,
           description: 'Rails Stripe customer',
           currency: 'eur'
         }
       )
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_cart_charge_path(cart.id)
+    redirect_to new_cart_charge_path(@cart.id)
   end # Amount in cents
+
+
 end
